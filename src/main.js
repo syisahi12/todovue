620ee183154621f11de73b8031de363c665bb50f
Syifa Saif Hidayat <syifa.sh404@students.amikom.ac.id>
2022-07-03 19:35:46 +0700

    ini udah nambah global css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/global/body.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/js/bootstrap.min'
 createApp(App).use(router).mount('#app')

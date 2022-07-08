b56006f086d4e62ab9a3493adc7c37f1377e8860
Dew <system@192.168.137.43>
2022-07-08 16:04:19 +0700

    dari dewa

---
+++ src/views/About.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="landing">
+    <section class="showcase">
+      <header>
+        <router-link to="/"
+          ><h2 class="logo" style="text-transform: lowercase">
+            inget.in
+          </h2></router-link
+        >
+      </header>
+      <div class="overlay">
+        <div class="about-section">
+          <div class="inner-width">
+            <h1>About Us</h1>
+            <div class="border"></div>
+            <div class="about-section-row">
+              <div class="about-section-col">
+                <div class="about">
+                  <br />
+                  <p>
+                    Menjadi Lebih Produktif Bersama Inget.In, Sesuai Dengan
+                    Slogan Kami Disini Kami Menawarkan Kemudahan Untuk User
+                    Dengan Menggunakan Apps Kami User Bisa Membuat List Yang
+                    AKan DiKerjakan, Dan Itu Akan Membantu User Untuk Me
+                    Manajement Waktu Da Lain-Lain
+                  </p>
+                </div>
+              </div>
+              <div class="about-section-col">
+                <div class="skills">
+                  <div class="skill">
+                    <div class="title">CSS</div>
+                    <div class="progress">
+                      <div class="progress-bar p1"><span>42%</span></div>
+                    </div>
+                  </div>
+
+                  <div class="skill">
+                    <div class="title">Vue</div>
+                    <div class="progress">
+                      <div class="progress-bar p2"><span>38%</span></div>
+                    </div>
+                  </div>
+
+                  <div class="skill">
+                    <div class="title">JavaScript</div>
+                    <div class="progress">
+                      <div class="progress-bar p3"><span>12%</span></div>
+                    </div>
+                  </div>
+
+                  <div class="skill">
+                    <div class="title">HTML</div>
+                    <div class="progress">
+                      <div class="progress-bar p4"><span>8%</span></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="about-section-row">
+              <div class="about-section-col">
+                <ul class="social">
+                  <li>
+                    <a href="#"><img src="../assets/images/facebook.png" /></a>
+                  </li>
+                  <li>
+                    <a href="#"><img src="../assets/images/twitter.png" /></a>
+                  </li>
+                  <li>
+                    <a href="#"><img src="../assets/images/instagram.png" /></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style src="../assets/style/about.css" scoped>
+</style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="landing">
+    <header>
+      <router-link to="/"
+        ><h2 class="logo" style="text-transform: lowercase">
+          inget.in
+        </h2></router-link
+      >
+    </header>
+    <div class="overlay">
+      <div class="contact-section">
+        <h1>Contact Us</h1>
+        <div class="border"></div>
+        <form class="contact-form">
+          <input type="text" class="contact-form-text" placeholder="Nama" />
+          <input type="email" class="contact-form-text" placeholder="Email" />
+          <input type="text" class="contact-form-text" placeholder="No.telp" />
+          <textarea class="contact-form-text" placeholder="Pesan"></textarea>
+          <input type="submit" class="contact-form-btn" value="Kirim" />
+        </form>
+      </div>
+    </div>
+    <div class="about-section-row">
+      <div class="about-section-col">
+        <ul class="social">
+          <li>
+            <a href="#"><img src="../assets/images/facebook.png" /></a>
+          </li>
+          <li>
+            <a href="#"><img src="../assets/images/twitter.png" /></a>
+          </li>
+          <li>
+            <a href="#"><img src="../assets/images/instagram.png" /></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style src="../assets/style/contact.css" scoped>
+</style>
